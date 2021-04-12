a1d4e393f16ba1fa166abce9d1a6bf5f2a58a250
Mohit Singh <mohitsingh1930@gmail.com>
2021-04-12 11:11:50 +0530

    minor bug fixed

--- source.js
+++ source.js
@@ -97,7 +97,7 @@ function handleBtn(event) {
   if(event instanceof KeyboardEvent) {
     btn = keyMap[event.key];
   } else {
-    btn = event.targed.id;
+    btn = event.target.id;
   }
 
   switch(btn){
@@ -154,5 +154,23 @@ sequenceMade = [];
 }
 
 
-// key bindings to play with keyboard
-
+// let promise = fetch(url, {
+//   method: "GET", // POST, PUT, DELETE, etc.
+//   headers: {
+//     // the content type header value is usually auto-set
+//     // depending on the request body
+//     "Content-Type": "text/plain;charset=UTF-8"
+//   },
+//   body: undefined, // string, FormData, Blob, BufferSource, or URLSearchParams
+//   referrer: "about:client", // or "" to send no Referer header,
+//   // or an url from the current origin
+//   referrerPolicy: "no-referrer-when-downgrade", // no-referrer, origin, same-origin...
+//   mode: "cors", // same-origin, no-cors
+//   credentials: "same-origin", // omit, include
+//   cache: "default", // no-store, reload, no-cache, force-cache, or only-if-cached
+//   redirect: "follow", // manual, error
+//   integrity: "", // a hash, like "sha256-abcdef1234567890"
+//   keepalive: false, // true
+//   signal: undefined, // AbortController to abort request
+//   window: window // null
+// });
